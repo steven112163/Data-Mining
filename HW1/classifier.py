bd4a16fd6362e039e862f50ce8e1d9d9824921bf
steven112163 <steven112163@gmail.com>
2020-11-11 14:59:55 +0800

    Add cv and knn

--- HW1/classifier.py
+++ HW1/classifier.py
@@ -1,6 +1,10 @@
 import pandas as pd
-from sklearn import preprocessing as pre
-from sklearn import feature_selection as fs
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
+from sklearn.model_selection import StratifiedKFold
+from sklearn.neighbors import KNeighborsClassifier
+from typing import List
+from statistics import mean
 import pprint
 
 
@@ -19,8 +23,10 @@ def info_fixer(training_info: pd.DataFrame) -> pd.DataFrame:
 
     # Assign unique values to distinct values in the column
     new_info['Comorbidities'] = new_info.groupby('Comorbidities').ngroup()
-    new_info['Antibiotics'] = new_info.groupby('Antibiotics').ngroup()
-    new_info['Bacteria'] = new_info.groupby('Bacteria').ngroup()
+
+    # Assign 0 if it's 0, 1 if it's a string
+    new_info['Antibiotics'] = new_info.groupby('Antibiotics').ngroup().astype(bool).astype(int)
+    new_info['Bacteria'] = new_info.groupby('Bacteria').ngroup().astype(bool).astype(int)
 
     return new_info
 
@@ -38,31 +44,73 @@ def tpr_fixer(training_tpr: pd.DataFrame) -> pd.DataFrame:
     patients = sectors.mean()
 
     # Standardize all columns
-    patients[['T', 'P', 'R', 'NBPS', 'NBPD']] = pre.StandardScaler().fit_transform(
+    patients[['T', 'P', 'R', 'NBPS', 'NBPD']] = StandardScaler().fit_transform(
         patients[['T', 'P', 'R', 'NBPS', 'NBPD']])
 
     return patients
 
 
-def compute_mutual_information(training_data: pd.DataFrame) -> None:
+def feature_selection(training_data: pd.DataFrame, training_target: pd.Series, k) -> List[str]:
     """
     Compute and show all mutual information between features and target
     :param training_data: training data set
+    :param training_target: training target
+    :param k: number of required features
+    :return: List of feature names
+    """
+    # Use mutual information to select top k features
+    list_of_col = SelectKBest(mutual_info_classif, k=k).fit(training_data, training_target).get_support(indices=True)
+    features = list(map(list(training_data).__getitem__, list_of_col))
+
+    return features
+
+
+def cross_validator(training_data: pd.DataFrame, training_target: pd.Series) -> None:
+    """
+    Use cross validation to test each model
+    :param training_data: training data set
+    :param training_target: training_target
     :return: None
     """
-    all_info = {}
-    list_of_targets = tr_data['Target'].values
-    for col in list(training_data):
-        if col != 'No' and col != 'Target':
-            all_info[col] = fs.mutual_info_classif(tr_data[col].values.reshape(-1, 1), list_of_targets, n_neighbors=2)[
-                0]
+    skf = StratifiedKFold()
+    acc = []
+    for train_index, test_index in skf.split(training_data, training_target):
+        # Get training set and testing set
+        data_train, target_train = training_data.iloc[train_index.tolist()], training_target.iloc[train_index.tolist()]
+        data_test, target_test = training_data.iloc[test_index.tolist()], training_target.iloc[test_index.tolist()]
 
-    # Use T, R, NBPS, NBPD, Comorbidities from output?
-    for k, e in all_info.items():
-        if len(k) > 5:
-            print(f'{k}: \t{e}')
-        else:
-            print(f'{k}:\t\t{e}')
+        # Use training set to select features
+        features = feature_selection(data_train, target_train, 2)
+
+        # Use model to train and predict
+        acc.append(k_nearest_neighbors(data_train, target_train, data_test, target_test, features))
+    print(acc)
+    print(mean(acc))
+
+
+def k_nearest_neighbors(data_train: pd.DataFrame, target_train: pd.Series, data_test: pd.DataFrame,
+                        target_test: pd.Series, features: List[str]) -> float:
+    """
+
+    :param data_train: training data set
+    :param target_train: training target
+    :param data_test: testing data set
+    :param target_test: testing target
+    :param features: selected features
+    :return: accuracy
+    """
+    # Train and predict
+    neigh = KNeighborsClassifier()
+    neigh.fit(data_train[features], target_train)
+    prediction = neigh.predict(data_test[features]).tolist()
+
+    # Get accuracy
+    diff = 0
+    for idx, target in enumerate(target_test):
+        if prediction[idx] == target:
+            diff += 1
+
+    return float(diff) / len(prediction)
 
 
 if __name__ == '__main__':
@@ -84,4 +132,10 @@ if __name__ == '__main__':
     tr_data = pd.merge(tr_info, tr_tpr, on='No')
     # print(tr_data)
 
-    compute_mutual_information(tr_data)
+    # Get training target
+    tr_target = tr_data['Target'].copy()
+    del tr_data['Target']
+    del tr_data['No']
+
+    # print(feature_selection(tr_data, tr_target, 2))
+    cross_validator(tr_data, tr_target)
